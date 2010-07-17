4fc404dc6c0ca26b7d40bfa375dab60ed5e0951c
Robin Wilson <r.t.wilson@rmplc.co.uk>
2010-07-17 20:13:36 +0100

    Added to ProcessDunes.py to give simple statistics:
    * Mean dune length
    * Total dune length
    * Number of dunes
    
    Have included code to calculate Nearest Neighbour, but haven't
    done conversion to points yet.

--- ProcessDunes.py
+++ ProcessDunes.py
@@ -1,10 +1,6 @@
-# RasterToPolyline_sample.py
-# Description: 
-#   Converts a raster to polyline features.
-# Requirements: None
-# Author: ESRI
-# Date: Oct 20, 2005
-# Import system modules
+# Code written by Robin Wilson (robin@rtwilson.com)
+# to process dune crests created through processing DECAL
+# output files using IDL
 import arcgisscripting
 import os
 
@@ -13,11 +9,11 @@ def CalculateLength(PolylineFeatures):
     gp.AddField(PolylineFeatures, "Length", "double")
 
     print "Getting descriptions"
-    desc = gp.Describe(OutPolylineFeatures)
+    desc = gp.Describe(PolylineFeatures)
     shapeField = desc.ShapeFieldName
 
     print "Getting cursor"
-    rows = gp.UpdateCursor(OutPolylineFeatures)
+    rows = gp.UpdateCursor(PolylineFeatures)
     row = rows.Next()
     gp.AddMessage("  Calculating values" + "\n")
 
@@ -31,22 +27,99 @@ def CalculateLength(PolylineFeatures):
     del rows
     return
 
+def Mean(inputData, FieldName):
+    # Execute the Summary Statistics tool using the MEAN option
+    gp.Statistics_analysis(inputData, "mean_tmp", FieldName + " MEAN")
+    # Get a list of fields from the new in-memory table.
+    flds = gp.ListFields("mean_tmp")
+    # Retrieve the field with the mean value.
+    fld = flds.Next()
+    while fld:
+        if fld.Name.__contains__("MEAN_"):
+            break
+        fld = flds.Next()
+    # Open a Search Cursor using field name.
+    rows = gp.SearchCursor("mean_tmp", "", "", fld.Name)
+    #Get the first row and mean value.
+    row = rows.Next()
+    return row.GetValue(fld.Name)
+
+def Sum(inputData, FieldName):
+    # Execute the Summary Statistics tool using the MEAN option
+    gp.Statistics_analysis(inputData, "mean_tmp", FieldName + " SUM")
+    # Get a list of fields from the new in-memory table.
+    flds = gp.ListFields("mean_tmp")
+    # Retrieve the field with the mean value.
+    fld = flds.Next()
+    while fld:
+        if fld.Name.__contains__("SUM_"):
+            break
+        fld = flds.Next()
+    # Open a Search Cursor using field name.
+    rows = gp.SearchCursor("mean_tmp", "", "", fld.Name)
+    #Get the first row and mean value.
+    row = rows.Next()
+    return row.GetValue(fld.Name)
+
+def Count(inputData, FieldName):
+    # Execute the Summary Statistics tool using the MEAN option
+    gp.Statistics_analysis(inputData, "mean_tmp", FieldName + " COUNT")
+    # Get a list of fields from the new in-memory table.
+    flds = gp.ListFields("mean_tmp")
+    # Retrieve the field with the mean value.
+    fld = flds.Next()
+    while fld:
+        if fld.Name.__contains__("COUNT_"):
+            break
+        fld = flds.Next()
+    # Open a Search Cursor using field name.
+    rows = gp.SearchCursor("mean_tmp", "", "", fld.Name)
+    #Get the first row and mean value.
+    row = rows.Next()
+    return row.GetValue(fld.Name)
+
+# ----------------------------------------------------------------
+# Main Script Starts Here...
+# ----------------------------------------------------------------
+
+# Set input details
+InRaster = "C:\Documents and Settings\Robin Wilson\My Documents\_Academic\GISTest\TestOutput2.tif"
+PolylineFilename = "DunesExportTest.shp"
+SubsetFilename = "Subset.shp"
 
 # Create the Geoprocessor object
 gp = arcgisscripting.create()
 
-# Set local variables
-InRaster = "C:\Documents and Settings\Robin Wilson\My Documents\_Academic\GISTest\TestOutput2.tif"
-OutPolylineFeatures = "C:/DunesExportTest.shp"
+gp.OverWriteOutput = 1
 
-#os.remove(OutPolylineFeatures)
+gp.workspace = "C:\TestArcWorkspace"
+
+print "Finished initialising"
+
 
 print "Converting Raster -> Polyline"
 
 # Process: RasterToPolyline_conversion
-gp.RasterToPolyline_conversion(InRaster, OutPolylineFeatures, "ZERO", 0, "SIMPLIFY", "Value")
+gp.RasterToPolyline_conversion(InRaster, PolylineFilename, "ZERO", 0, "SIMPLIFY", "Value")
+
+print "Calculating length"
+CalculateLength(PolylineFilename)
+
+# Make a feature layer from the polylines
+gp.MakeFeatureLayer(PolylineFilename,"Polyline_lyr")
+
+print "Subsetting by length"
+gp.SelectLayerByAttribute("Polyline_lyr", "NEW_SELECTION", " \"Length\" > 15 ")
+gp.CopyFeatures("Polyline_lyr", SubsetFilename)
+
+print "Mean Length: ", Mean(SubsetFilename, "Length")
+print "Total Length: ", Sum(SubsetFilename, "Length")
+print "Number of dunes: ", Count(SubsetFilename, "Length")
 
 
-print "Trying to subset..."
-CalculateLength(OutPolylineFeatures)
+# Do Nearest Neighbour calculation
+nn_output = gp.AverageNearestNeighbor_stats(SubsetFilename, "Euclidean Distance", "false", "#")
+print nn_output
+
+
 
